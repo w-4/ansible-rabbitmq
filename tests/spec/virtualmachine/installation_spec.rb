ededa0e0b3ddaeaa5e804f76798ccbe8bb3f66da
Carlos Martins <cmartins@stone.com.br>
2017-03-07 20:19:30 -0300

    Adding tests with serverspec

--- tests/spec/virtualmachine/installation_spec.rb
+++ tests/spec/virtualmachine/installation_spec.rb
@@ -4,3 +4,7 @@ describe service('rabbitmq-server') do
   it { should be_enabled }
   it { should be_running }
 end
+
+describe package('rabbitmq-server') do
+  it { should be_installed.with_version('3.3.5') }
+end
