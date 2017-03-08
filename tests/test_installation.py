cf7f1608fda854edd491da6148e2ec2e98f9ce9c
Carlos Martins <cmartins@stone.com.br>
2017-03-08 20:23:55 -0300

    Tests for installation

--- tests/test_installation.py
+++ tests/test_installation.py
@@ -5,12 +5,17 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_rabbitmq_is_installed(Package):
-    nginx = Package("rabbitmq-server")
-    assert nginx.is_installed
-    assert nginx.version.startswith("3.3.5")
+    rabbitmq = Package("rabbitmq-server")
+    assert rabbitmq.is_installed
+    assert rabbitmq.version.startswith("3.3.5")
+
+
+def test_earlang_is_installed(Package):
+    erlang = Package("erlang")
+    assert erlang.is_installed
 
 
 def test_rabbitmq_running_and_enabled(Service):
-    nginx = Service("rabbitmq-server")
-    assert nginx.is_enabled
-    assert nginx.is_running
+    rabbitmq = Service("rabbitmq-server")
+    assert rabbitmq.is_enabled
+    assert rabbitmq.is_running
