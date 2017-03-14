da34961a0d1a207106dc875015e0602c1949ab5a
Carlos Martins <cmartins@stone.com.br>
2017-03-14 16:55:03 -0300

    Ensuring users are present or removed

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -11,3 +11,13 @@ def test_rabbitmq_config_file_permissions_must_be_644(File):
     assert rabbitmq_config.user == 'root'
     assert rabbitmq_config.group == 'rabbitmq'
     assert rabbitmq_config.mode == 0644
+
+
+def test_rabbitmq_guest_user_not_present(User):
+    guest_user = User("guest")
+    assert not guest_user.exists
+
+
+def test_rabbitmq_rabbit_user_present(User):
+    rabbitmq_user = User("rabbitmq")
+    assert rabbitmq_user.exists
