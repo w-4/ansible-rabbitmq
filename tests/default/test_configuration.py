23ba4514afeb005b56d9de317970800777dcfcf7
Paulo Mariano <paulo.azevedo@stone.com.br>
2020-07-07 20:19:19 -0300

    fix bug in tests

--- tests/default/test_configuration.py
+++ tests/default/test_configuration.py
@@ -6,7 +6,15 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_rabbitmq_config_file_permissions_must_be_644(host):
-    rabbitmq_config = host.file("/etc/rabbitmq/rabbitmq.config")
+    ansible_vars = host.ansible.get_variables()
+    rabbitmq_conf_file = "/etc/rabbitmq/rabbitmq.config"
+
+    if (ansible_vars['rabbitmq_clustering_enabled']):
+        with host.sudo():
+            rabbitmq_config = host.file(rabbitmq_conf_file)
+    else:
+        rabbitmq_config = host.file(rabbitmq_conf_file)
+
     assert rabbitmq_config.exists
     assert rabbitmq_config.is_file
     assert rabbitmq_config.user == 'rabbitmq'
@@ -15,12 +23,28 @@ def test_rabbitmq_config_file_permissions_must_be_644(host):
 
 
 def test_rabbitmq_guest_user_not_present(host):
-    rabbitmq_users = host.run("rabbitmqctl list_users")
+    ansible_vars = host.ansible.get_variables()
+    rabbitmqctl_list_users_cmd = "rabbitmqctl list_users"
+
+    if (ansible_vars['rabbitmq_clustering_enabled']):
+        with host.sudo():
+            rabbitmq_users = host.run(rabbitmqctl_list_users_cmd)
+    else:
+        rabbitmq_users = host.run(rabbitmqctl_list_users_cmd)
+
     assert "guest" not in rabbitmq_users.stdout
 
 
 def test_rabbitmq_rabbit_user_present(host):
-    rabbitmq_users = host.run("rabbitmqctl list_users")
+    ansible_vars = host.ansible.get_variables()
+    rabbitmqctl_list_users_cmd = "rabbitmqctl list_users"
+
+    if (ansible_vars['rabbitmq_clustering_enabled']):
+        with host.sudo():
+            rabbitmq_users = host.run(rabbitmqctl_list_users_cmd)
+    else:
+        rabbitmq_users = host.run(rabbitmqctl_list_users_cmd)
+
     assert "admin" in rabbitmq_users.stdout
 
 
